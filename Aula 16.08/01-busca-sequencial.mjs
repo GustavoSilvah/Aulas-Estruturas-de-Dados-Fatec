78d491d7c0161015a25c9b943dd23553413602ee
Gustavo da Silva <gustavosilva.gs2001@hotmail.com>
2021-08-17 12:43:45 -0300

    17/08 - Término da Busca Sequencial.

--- Aula 16.08/01-busca-sequencial.mjs
+++ Aula 16.08/01-busca-sequencial.mjs
@@ -27,7 +27,30 @@ console.log('Posição de abacate:', buscaSequencial(frutas, 'abacate'))
 // Número de comparações em um vetor de n elementos:
 // Quando valorBusca é encontrado: pos + 1
 // Quando valorBusca não é encontrado: n
-
+console.log("================================")
 import { nomes } from "../data/vetor-nomes.mjs"
 
+console.time('Buscando GUSTAVO...') // Dispara a contagem de tempo
 console.log('Procura de GUSTAVO', buscaSequencial(nomes, 'GUSTAVO'))
+console.timeEnd('Buscando GUSTAVO...')  // Para a contagem de tempo
+
+// A primeira busca demora mais, pois ela carrega os dados na memória
+console.log("================================")
+console.time('Buscando FAUSTO...') // Dispara a contagem de tempo
+console.log('Procura de FAUSTO', buscaSequencial(nomes, 'FAUSTO'))
+console.timeEnd('Buscando FAUSTO...')  // Para a contagem de tempo
+
+console.log("================================")
+console.time('Buscando VERONICA...') // Dispara a contagem de tempo
+console.log('Procura de VERONICA', buscaSequencial(nomes, 'VERONICA'))
+console.timeEnd('Buscando VERONICA...')  // Para a contagem de tempo
+
+console.log("================================")
+console.time('Buscando BERNARDO...') // Dispara a contagem de tempo
+console.log('Procura de BERNARDO', buscaSequencial(nomes, 'BERNARDO'))
+console.timeEnd('Buscando BERNARDO...')  // Para a contagem de tempo
+
+console.log("================================")
+console.time('Buscando ORKUTILSON...') // Dispara a contagem de tempo
+console.log('Procura de ORKUTILSON', buscaSequencial(nomes, 'ORKUTILSON'))
+console.timeEnd('Buscando ORKUTILSON...')  // Para a contagem de tempo
