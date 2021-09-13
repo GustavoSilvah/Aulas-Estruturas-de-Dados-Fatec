1643677d2a7ad8bbbb2be629942c16ca04b65f9a
Gustavo da Silva <gustavosilva.gs2001@hotmail.com>
2021-09-13 09:20:08 -0300

    (13/09)

---
+++ Aula 13.09/09-recursividade.mjs
@@ -0,0 +1,49 @@
+/* 
+    Fatorial de um número natural é igual a ele próprio
+    multiplicado por todos os seus antecessores
+
+    5! = 5 * 4 * 3 * 2 * 1 (120)
+    4! = 4 * 6 * 2 * 1 (24)
+    3! = 3 * 2 * 1 (6)
+    2! = 2 * 1 (2)
+
+    Casos especiais:
+    1! = 1
+    0! = 1
+
+    Expressando recursivamente:
+    5! = 5 * 4!
+    4! = 4 * 3!
+    3! = 3 * 2!
+    2! = 2 * 1!
+
+    fatorial(n) = n * fatoria(n - 1), para qualquer n > 1
+
+    Diz-se que uma definição é recursiva quando a definição aparece
+    dois lados da igualdade
+
+    Para cada algoritmo recursivo, existe o correspondente iterativo.
+    No entanto, a versão iterativa pode ser mais extensa e difícil de
+    implementar que a versão recursiva.
+*/
+
+function fatorial(n) {
+    let resultado = 1;
+    for(let i = n; i > 1; i--) resultado *= i
+    return resultado
+}
+
+console.log('Fatorial de 5:', fatorial(5))
+
+// Implementação RECURSIVA de uma função que calcula o fatorial
+// de um número. Note que existe uma chamada à própria funçãi
+// dentro dela
+
+// Condição de saída: é uma situação em que a função recursiva
+// não chama a si mesma
+function fatorialRecursivo(n) {
+    if(n <= 1) return 1     // - > Condição de saída
+    return n * fatorialRecursivo(n - 1)
+}
+
+console.log('Fatorial Recursivo de 5:', fatorialRecursivo(5))
